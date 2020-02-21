53ce1a6b6d2ee8136140d67d97a5498e4e4d5749
LIZBETHJAICO <lizbethjaico@gmail.com>
2020-02-21 18:35:35 -0500

    conectando db in mongodb ✅

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const mongoClient = require('mongodb').MongoClient;
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
 const pkg = require('./package.json');
 
+
 const { port, dbUrl, secret } = config;
 const app = express();
+
+
+mongoClient.connect(dbUrl, { useUnifiedTopology: true }, (err, db) => {
+  console.log(db);
+  console.log('conexion satisfactora');
+});
 
 // TODO: Conección a la BD en mogodb
 
@@ -17,6 +25,7 @@ app.set('pkg', pkg);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(authMiddleware(secret));
+// CONECCION
 
 // Registrar rutas
 routes(app, (err) => {
