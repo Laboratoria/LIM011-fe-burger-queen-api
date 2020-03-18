d2f1af641e56c61f1e58066b3a0345599c27c030
LIZBETHJAICO <lizbethjaico@gmail.com>
2020-03-17 20:20:30 -0500

    controller producto refactorizado

--- controller/ProductsController.js
+++ controller/ProductsController.js
@@ -3,52 +3,44 @@ const db = require('../conection/connection');
 const { pagination } = require('./utils');
 
 module.exports = {
-  createProduct: async (req, res, next) => {
+  createProduct: async (req, resp, next) => {
     const {
-      name, price, imagen, type,
+      name, price, image = '', type = '',
     } = req.body;
+    const collectionProducts = (await db()).collection('products');
     if (!name || !price) {
       return next(400);
     }
-    const productsCollection = (await db()).collection('products');
-    const newProduct = (await productsCollection.insertOne({
+    const productId = (await collectionProducts.insertOne({
       name,
       price,
-      imagen,
+      image,
       type,
       dateEntry: new Date(),
     })).insertedId;
-    // console.log('id', typeof newProduct);
-    const product = await productsCollection.findOne(newProduct);
-    // console.log('product', product);
-    res.send(product);
+    const product = await collectionProducts.findOne({ _id: ObjectId(productId) });
+    resp.send(product);
   },
   getProductById: async (req, resp, next) => {
     const { productId } = req.params;
-    if (!ObjectId.isValid(productId)) {
-      return next(404);
-    }
-    const query = { _id: ObjectId(productId) };
+    const query = (ObjectId.isValid(productId) ? { _id: ObjectId(productId) } : { _id: productId });
     const productsCollection = (await db()).collection('products');
     const checkProduct = await productsCollection.findOne(query);
     if (!checkProduct) {
       return next(404);
     }
-    /* console.log('verificando', checkProduct);
-    console.log('is valid', ObjectId.isValid(productId)); */
     resp.send(checkProduct);
   },
   getProducts: async (req, resp, next) => {
     const limit = parseInt(req.query.limit, 10) || 10;
     const page = parseInt(req.query.page, 10) || 1;
     const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
     const productsCollection = (await db()).collection('products');
     const allProducts = await productsCollection.find().skip(startIndex).limit(limit).toArray();
-    // console.log('todos', allProducts);
+    console.log('todos', allProducts);
     const numberOfProducts = await productsCollection.find().count();
     const numberOfPages = Math.ceil(numberOfProducts / limit);
-    // console.log('number', numberOfProducts);
+    console.log('number', numberOfProducts);
     const linksHeader = pagination('products', page, numberOfPages, limit);
     resp.set('link', `${linksHeader.first},${linksHeader.last},${linksHeader.prev},${linksHeader.next}`);
     resp.send(allProducts);
@@ -58,10 +50,7 @@ module.exports = {
     const {
       name, price, imagen, type,
     } = req.body;
-    if (!ObjectId.isValid(productId)) {
-      return next(404);
-    }
-    const query = { _id: ObjectId(productId) };
+    const query = (ObjectId.isValid(productId) ? { _id: ObjectId(productId) } : { _id: productId });
     const productsCollection = (await db()).collection('products');
     const checkProduct = await productsCollection.findOne(query);
     if (!checkProduct) {
